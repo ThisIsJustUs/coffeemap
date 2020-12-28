873ba555c5b3ef82fd4ad998b28f553f69ce829a
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2020-12-28 13:03:45 +0100

    Added Web

--- server/src/middleware/isLoggedIn.ts
+++ server/src/middleware/isLoggedIn.ts
@@ -4,7 +4,6 @@ import { NextFunction, Request, Response } from 'express';
 // A middleware which checks whether a user is authenticated.
 // The function uses the serialise/deserialise function provided by Passport
 export default (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.isAuthenticated());
     if (!req.isAuthenticated()) {
         return res.redirect('/auth/unauthorized');
     }
