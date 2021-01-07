2bf180fe4396dd02b754161418fb813af1ff9434
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2021-01-07 16:52:31 +0100

    Menu Icon Button

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -17,7 +17,7 @@ export const NavBar: React.FC<NavBarProps> = ({ activePage }) => {
                         <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
                             {/* Mobile Menu Button */}
                             <button
-                                className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+                                className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
                                 aria-expanded='false'
                                 onClick={() => setIsOpen(!isOpen)}
                             >
@@ -187,19 +187,19 @@ export const NavBar: React.FC<NavBarProps> = ({ activePage }) => {
              */}
                 <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`}>
                     <div className='px-2 pt-2 pb-3 space-y-1 font-serif'>
-                        <Link href='/dashboard'>
+                        <Link href='/overview'>
                             <a className='bg-gray-100 block px-3 py-2 rounded-md text-base font-medium'>
                                 Dashboard
                             </a>
                         </Link>
-                        <Link href='/map'>
+                        <Link href='/login'>
                             <a className='text-gray-700 hover:bg-gray-100 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                                Map
+                                Login
                             </a>
                         </Link>
-                        <Link href='/settings'>
+                        <Link href='/register'>
                             <a className='text-gray-700 hover:bg-gray-100 hover:text-white block px-3 py-2 rounded-md text-base font-medium'>
-                                Settings
+                                Register
                             </a>
                         </Link>
                     </div>
