f7cfa7f2281210b0e11b3d00d26cae57d8dec013
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2021-01-11 00:01:28 +0100

    Env variables for web using GitHub ref

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,2 +1,4 @@
 export const API_URL =
-    process.env.NEXT_PUBLIC_API_URL || process.env.STAGING_PUBLIC_API_URL;
+    process.env.VERCEL_GITHUB_COMMIT_REF == 'dev'
+        ? process.env.NEXT_PUBLIC_API_URL
+        : process.env.STAGING_PUBLIC_API_URL;
