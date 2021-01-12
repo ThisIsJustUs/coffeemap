06e7370df9a67f0154f4a62e443c98d72c35f6ef
Justus Hebenstreit <hebenstreit.justus@gmail.com>
2021-01-13 00:11:01 +0100

    New Github env variable

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,5 +1,5 @@
 const API_URL =
-    process.env.VERCEL_GITHUB_COMMIT_REF === 'master'
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF === 'master'
         ? process.env.NEXT_PUBLIC_API_URL
         : process.env.STAGING_PUBLIC_API_URL;
 
